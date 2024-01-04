50053cd9565d8eb29eab9b69b2dba0bb24942e56
Dharmi <dharmip060@gmail.com>
2024-01-04 18:58:57 +0530

    integration

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-import NavBar from '../Components/NavBar'
-import image1 from '../assets/home-1.jpg'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import NavBar from '../Components/NavBar';
+import image1 from '../assets/home-1.jpg';
+
+
+
+const API_URL = import.meta.env.VITE_API_URL_SELLER
 
 const Home = () => {
+    const [allProducts, setAllProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch products from the backend when the component mounts
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/sell`); 
+        setAllProducts(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
     return (
         <div className='bg-primary'>
             <div>
@@ -16,11 +36,27 @@ const Home = () => {
                     <button className='p-4 border-2 border-white rounded-full hover:bg-secondary'>Get Started</button>
                 </div>
                 <div className='md:w-1/2 justify-end md:ml-16'>
-                    <img src={image1} className='w-[400px] h-[400px]' />
+                    <img src={image1} className='w-[6
+                      00px] h-[400px]' />
                 </div>
-
             </div>
 
+            <div>
+            <div className="p-4">
+          <div className="flex overflow-x-auto whitespace-nowrap w-full gap-4">
+            {allProducts.map((product) => (
+              <ProductCard
+                key={product._id}
+          
+                name={product.name}
+                description={product.description}
+                price={product.price}
+                id={product._id}
+              />
+            ))}
+          </div>
+        </div>
+            </div>
         </div>
     )
 }
