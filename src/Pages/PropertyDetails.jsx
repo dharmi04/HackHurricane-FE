cb379edacdf9a5bb30bcfd621736869ef5361cb2
Mansi Rank <rankmansi0101@gmail.com>
2024-01-04 23:47:02 +0530

    bookappointment

--- src/Pages/PropertyDetails.jsx
+++ src/Pages/PropertyDetails.jsx
@@ -14,12 +14,13 @@ const PropertyDetails = () => {
   };
 
   return (
-    <div>
-      <h2>Property Details Page</h2>
-      <p>Property ID: {id}</p>
-      <p>Property Type: {PropertyType}</p>
-
-      <button onClick={handleBookAppointment}>Book Appointment</button>
+    <div style={{ background: '#603808', padding: '20px', color: 'white', fontSize: '24px', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }} className='h-screen'>
+  <div>
+    <h2 className='text-white mb-7'>Property Details Page</h2>
+    <p className='text-white'>Property ID: {id}</p>
+    <p className='text-white'>Property Type: {PropertyType}</p>
+    <button onClick={handleBookAppointment} className='text-white mt-3'>Book Appointment</button>
+  </div>
 
       {isDialogOpen && (
         <div
@@ -29,27 +30,34 @@ const PropertyDetails = () => {
             top: '50%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
-            background: 'white',
+            background: '#FFEDD8', // Dialog box color
             padding: '20px',
             border: '1px solid #ccc',
             boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+            borderRadius: '10px', // Rounded corners for the dialog box
             zIndex: '9999',
           }}
         >
-          <h3>Book an Appointment</h3>
+          <h3 className='mb-7 justify-center text-black'>
+            Book an Appointment
+          </h3>
           <form>
-            <label>
+            <label className='mb-7 justify-center text-black'>
               Email:
-              <input type="text" name="email" />
+              <input type="text" name="email" style={{ borderRadius: '8px', padding: '8px' }} className='mb-7 justify-center text-black ml-7'/>
             </label>
             <br />
-            <label>
+            <label className='mb-7 justify-center text-black'>
               Contact Number:
-              <input type="text" name="contactNumber" />
+              <input type="text" name="contactNumber" style={{ borderRadius: '8px', padding: '8px' }} className='mb-7 justify-center text-black ml-7'/>
             </label>
             <br />
-            <button type="submit">Confirm</button>
-            <button onClick={handleDialogClose}>Cancel</button>
+            <button type="submit" className='justify-center text-black'>
+              Confirm
+            </button>
+            <button onClick={handleDialogClose} style={{ borderRadius: '8px', padding: '8px' }} className='justify-center text-black ml-7'>
+              Cancel
+            </button>
           </form>
         </div>
       )}
